4756fd351d75ade315c6741897ec5df5917da4de
xdkillyou <xdkillyou@gmail.com>
2025-03-06 23:39:31 +0800

    create base style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
+  <div>
+    <!-- playgroud -->
+    <div un-bg="bg-default" un-m="b-20px">
+      <div un-w="25px" un-h="25px" un-bg="bg-hover"></div>
+      <div un-w="25px" un-h="25px" un-bg="bg-buyQuoteBar"></div>
+      <div un-w="25px" un-h="25px" un-bg="bg-sellQuoteBar"></div>
+      <div un-w="25px" un-h="25px" un-bg="bg-flashGreen"></div>
+      <div un-w="25px" un-h="25px" un-bg="bg-flashRed"></div>
+      <div un-color="text-default">Lorem ipsum dolor sit amet</div>
+      <div un-color="text-tableHead">Lorem ipsum dolor sit amet</div>
+      <div un-color="text-buyQuote">Lorem ipsum dolor sit amet</div>
+      <div un-color="text-sellQuote">Lorem ipsum dolor sit amet</div>
+    </div>
   <div></div>
+  </div>
 </template>
--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,33 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, presetAttributify, presetUno } from 'unocss'
 
 export default defineConfig({
-  // ...UnoCSS options
+  presets: [
+    presetAttributify({
+      prefix: 'un-',
+      prefixedOnly: true,
+    }),
+    presetUno(),
+    // ...custom presets
+  ],
+  theme: {
+    colors: {
+      bg: {
+        default: '#131B29',
+        hover: '#1E3059',
+        buyQuoteBar: 'rgba(16, 186, 104, 0.12)',
+        sellQuoteBar: 'rgba(255, 90, 90, 0.12)',
+        flashGreen: 'rgba(0, 177, 93, 0.5)',
+        flashRed: 'rgba(255, 91, 90, 0.5)',
+      },
+      text: {
+        default: '#F0F4F8',
+        tableHead: '#8698AA',
+        buyQuote: '#00B15D',
+        sellQuote: '#FF5B5A',
+      },
+    },
+  },
+  shortcuts: {
+    row: 'grid grid-cols-[25%_25%_50%]',
+  },
 })
