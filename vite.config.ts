393df14371c1b6bfe5d38370c1499f5919cd0b82
xdkillyou <xdkillyou@gmail.com>
2025-03-09 12:43:17 +0800

    add library & icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), UnoCSS()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    UnoCSS(),
+    svgLoader(),
+    AutoImport({
+      imports: ['vue'],
+      dts: true,
+    }),
+    Components({
+      dts: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
